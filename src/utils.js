c190bf93bf2bea114a0ec20174353190c5fd4540
KARTHICK KUMAR P <karthickkumar2005@gmail.com>
2025-05-14 09:34:35 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
+function sayHi() { console.log('Hi from shared library\!'); }
 function sayHello() { console.log('Hello from shared library\!'); }
 function sayGoodbye() { console.log("Goodbye from shared library!"); }
 function sayGoodnight() { console.log("Goodnight to devops!"); }
